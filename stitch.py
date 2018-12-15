0be3bf496e28b866e03f5034df0419ae3155ba68
Rahman <rahman@iflix.com>
2018-12-15 19:45:11 +1100

    Chapter stitcher

---
+++ stitch.py
@@ -0,0 +1,30 @@
+import glob
+from functools import reduce
+from PyPDF2 import PdfFileMerger, PdfFileReader
+from yaml import safe_load
+
+
+def to_list_of_pdf(chapter):
+  pdfs = []
+  pdfs += ([chapter['pdf']] if 'pdf' in chapter else [])
+  pdfs += [sub['pdf'] if 'pdf' in sub else None for sub in chapter['sub']]
+  return list(filter(lambda x: x is not None, pdfs))
+
+def main():
+  book_path = './output/kubernetes.io-docs.pdf'
+  stream = open('./output/index.yaml')
+  index = safe_load(stream)
+  merger = PdfFileMerger()
+
+  mapped = map(to_list_of_pdf, index)
+  filtered = filter(lambda x: len(x)>0, mapped)
+  all_pdfs = reduce(lambda x,y: x+y, filtered, [])
+
+  for pdf in all_pdfs:
+    merger.append(PdfFileReader(pdf))
+
+  with open(book_path, 'wb') as f:
+    merger.write(f)
+
+if __name__ == '__main__':
+    main()
