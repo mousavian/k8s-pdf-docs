9b07db941143abe42f4ce9561d49400428f2e06e
Rahman <rahman@iflix.com>
2018-12-16 12:38:08 +1100

    Stitching in chunks to prevent exceeding max recursion depth

--- stitch.py
+++ stitch.py
@@ -7,17 +7,30 @@ def main():
   book_path = './output/kubernetes.io-docs.pdf'
   stream = open('./output/index.yaml')
   index = safe_load(stream)
-  merger = PdfFileMerger()
 
   mapped = map(to_list_of_pdf, index)
   filtered = filter(lambda x: len(x)>0, mapped)
   all_pdfs = reduce(lambda x,y: x+y, filtered, [])
+  N = 50
+  chunks = [all_pdfs[x:x+N] for x in range(0, len(all_pdfs), N)]
+  for i, chunk in enumerate(chunks):
+    merger = PdfFileMerger()
+    for pdf in chunk:
+      merger.append(PdfFileReader(pdf))
 
-  for pdf in all_pdfs:
-    merger.append(PdfFileReader(pdf))
+    with open('/tmp/k8s-doc-chunk-%s'%i, 'wb') as f:
+      merger.write(f)
+    merger.close()
+
+  merger = PdfFileMerger()
+  for i, chunk in enumerate(chunks):
+    merger.append(PdfFileReader('/tmp/k8s-doc-chunk-%s'%i))
 
   with open(book_path, 'wb') as f:
-    merger.write(f)
+      merger.write(f)
+  merger.close()
+  return
+
 
 def to_list_of_pdf(chapter):
   pdfs = []
